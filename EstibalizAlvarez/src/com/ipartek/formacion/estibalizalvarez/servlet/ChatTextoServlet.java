ae3699ce03ad3a7c7c57c77b0ed252c2e1e2bd6b
esti <JAVA@PORT-Z14>
2017-04-27 19:37:54 +0200

    Ejercico de Chat hecho.

--- EstibalizAlvarez/src/com/ipartek/formacion/estibalizalvarez/servlet/ChatTextoServlet.java
+++ EstibalizAlvarez/src/com/ipartek/formacion/estibalizalvarez/servlet/ChatTextoServlet.java
@@ -20,11 +20,17 @@ public class ChatTextoServlet extends HttpServlet {
 		String usuario = (String) session.getAttribute("nombre");// EL nombre de usuario de inicio de sesion.
 
 		ServletContext application = getServletContext();// coges la bolsa global para que el texto que metas lo vea todo el mundo.
-		String texto1 = request.getParameter("texto");// para guardar texto escrito en "textoescrito" en la variable texto1.
-		application.setAttribute("textoGlobal", texto1);// se guarda en el textoGlobal el testo1 escrito anteriormente.
+		String texto1 = request.getParameter("texto");// para guardar texto escrito en "texto" en la variable texto1.
+
+		String textogrande = (String) application.getAttribute("textoGlobal");// sacas el texto de la bolsa.
+		if (textogrande == null)// si la primera vez es null
+			textogrande = usuario + texto1 + "<br/>";// sacame solo usuario y texto.
+		else
+			textogrande = textogrande + usuario + texto1 + "<br/>";// le sacas el texto existente y le añades usuario y texto nuevo.
+		application.setAttribute("textoGlobal", textogrande);// se guarda en el textoGlobal todo.
 
 		PrintWriter out = response.getWriter();// canal de escritura al navegador.
-		out.println(usuario + texto1);// aqui te saca el usuario y el texto que escribe.
+		out.println(textogrande);// aqui te saca el usuario y el texto que escribe.sacas todo el chat por pantalla.
 	}
 
 }
