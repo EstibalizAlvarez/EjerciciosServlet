91851d637adef9b8cfbb855ca00d2fccc014c5df
esti <JAVA@PORT-Z14>
2017-05-02 19:58:12 +0200

    ultimo

--- EstibalizAlvarez/src/com/ipartek/ejemplos/estibalizalvarez/controladores/LoginServlet.java
+++ EstibalizAlvarez/src/com/ipartek/ejemplos/estibalizalvarez/controladores/LoginServlet.java
@@ -16,6 +16,12 @@ import com.ipartek.ejemplos.estibalizalvarez.tipos.Usuario;
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	// Para indicar las rutas de las Páginas:
+	public static final String RUTA = "WEB-INF/Vistas/";
+	public static final String rutaPrincipal = RUTA + "principal.jsp";
+	public static final String rutaLogin = RUTA + "login.jsp";
+	public static final String rutaError = RUTA + "error.jsp";
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doPost(request, response);// el metodo doget llama al dopost.
 	}
@@ -34,21 +40,22 @@ public class LoginServlet extends HttpServlet {
 		UsuariosDAL usuarioDAL = new UsuariosDALFijo();
 
 		// Sólo para crear una base de datos falsa con el
-		// contenido de un usuario "javi", "lete"
+		// contenido de un usuario: "Pupi", contraseña: "brutus".
 		usuarioDAL.alta(new Usuario("Pupi", "brutus"));
 
 		boolean esValido = usuarioDAL.validar(usuario);
 
-		// Redirigir a una nueva vista
+		// Redirigir a una nueva vista.
 		if (esValido) {
 			request.getSession().setAttribute("usuario", usuario);// esto es para que te recuerde quien eres.
-			response.sendRedirect("principal.jsp");
+			response.sendRedirect(rutaPrincipal);
 		} else {
 			usuario.setErrores("El usuario y contraseña indtroduciodos no son válidos");// te sale el mensaje al introducir el usuario y pass incorrectos.
 			request.setAttribute("usuario", usuario);//
-			request.getRequestDispatcher("login.jsp").forward(request, response);// que te redireciones a la pagina de login.
+			request.getRequestDispatcher(rutaLogin).forward(request, response);// que te redireciones a la pagina de login.
 
 			// response.sendRedirect("error.jsp");//que te valla a la pagina error.
+			// response.sendRedirect("rutaError.jsp"); esto es lo mismo que el de arriba.
 		}
 	}
 }
